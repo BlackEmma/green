ac915c8328762dcb8bf81c7634ca13b42c7a1d74
Ekaterina Ekimova <psich.kawai@gmail.com>
2023-06-09 17:43:09 +0300

    add Header

---
+++ src/App/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../features/Header/Header';
+
+function App(): JSX.Element {
+  return (
+    <div className="App">
+     <Header />
+    </div>
+  );
+}
+
+export default App;
