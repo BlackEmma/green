c8a5ccb0ab7176ef526d44c0ae572756ff7b26ac
Ekaterina Ekimova <psich.kawai@gmail.com>
2023-06-09 22:19:32 +0300

    temporarily replace Main with ChatPage in App

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import Main from '../features/Main/Main';
+// import Main from '../features/Main/Main';
+import ChatPage from '../features/ChatPage/ChatPage';
+import style from './App.module.css';
 
 function App(): JSX.Element {
   return (
     <div className="App">
-     <Main />
+      <div className={style.container}>
+        <ChatPage />
+      </div>
     </div>
   );
 }
